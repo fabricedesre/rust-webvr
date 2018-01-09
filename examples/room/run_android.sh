9dce454225b87b81df98c56746789aa7fbaac300
Fabrice Desré <fabrice@desre.org>
2018-01-09 08:56:34 -0800

    remove examples

--- examples/room/run_android.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cargo build --target=arm-linux-androideabi --release
-cd ./android
-./gradlew installGearvrArmRelease
-adb shell am start -n com.rust.webvr/com.rust.webvr.MainActivity
-#logcat-color | egrep '(RustAndroid|art|DEBUG)'
